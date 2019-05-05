f3164e8909e8def0f34b0187f9884a8f84674590
Jose Ibanez <ibanez.j@gmail.com>
2019-05-05 14:48:47 +0200

    folder and no symlinks fix

--- deploy/publish.static.sh
+++ deploy/publish.static.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cd ..
 
 aws s3 sync ./sb s3://mybalcon/sb/
 aws s3 sync ./js s3://mybalcon/js/
 aws s3 sync ./vendor s3://mybalcon/vendor
-aws s3 sync ./html s3://mybalcon/html
+aws s3 sync --no-follow-symlinks ./html s3://mybalcon/html
 
-aws s3 cp html/index.html s3://mybalcon/
+aws s3 cp  html/index.html s3://mybalcon/
